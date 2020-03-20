78954563ad86f972512ab34800bd8f389374cdf1
JoonHyun <slio_7@naver.com>
2020-03-20 14:19:47 +0900

    Apply Material UI

--- src/components/info_me.js
+++ src/components/info_me.js
@@ -1,35 +1,19 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 
 class Info_me extends React.Component {
     render() {
         return (
-            <div>
-                <Me_Profile id = {this.props.id} image = {this.props.image} name = {this.props.name}/>
-                <Me_Info birthday = {this.props.birthday} gender = {this.props.gender} job = {this.props.job}/>
-            </div>
-        )
-    }
-}
-
-class Me_Profile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src = {this.props.image} alt = "profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class Me_Info extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src = {this.props.image} alt = "profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
