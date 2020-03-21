dc0bb62ba0d015f208c20a6737405e5921f28a41
JoonHyun <slio_7@naver.com>
2020-03-21 16:58:26 +0900

    Add API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Info_me from "./components/info_me";
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
+import CircularProgress from '@material-ui/core/CircularProgress'
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
@@ -17,15 +18,20 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1000
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 });
 
 class App extends Component {
   state = {
-    data: ""
+    data: "",
+    completed: 0 // 로딩 애니메이션의 초기값: 0, 0 ~ 100까지 게이지가 채워짐
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20); // 0.02초마다 progress함수가 실행됨
     this.callApi()
       .then(res => this.setState({data: res}))
       .catch(err => console.log(err));
@@ -35,6 +41,13 @@ class App extends Component {
     const response = await fetch('/api/info_me');
     const body = await response.json();
     return body;
+  }
+
+  // 애니메이션 함수
+  progress = () => {
+    const {completed} = this.state;
+    // completed가 100이되면 0으로 돌아가고 아닐 시 1씩 증가
+    this.setState({completed: completed >= 100 ? 0 : completed + 1})
   }
 
   render() {
@@ -66,7 +79,14 @@ class App extends Component {
               job = {i.job}
             />
             );
-          }) : "" }
+          }) :
+          // 데이터를 불러 오지 못할 경우(로딩페이지)
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+          }
           </TableBody>
         </Table>
       </Paper>
